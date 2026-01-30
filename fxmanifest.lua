82aaf7bb289affc2b2a15ec0e5dac36c34c47dff
Hast <hast.herish@yahoo.com>
2026-01-30 09:08:09 -0500

    File structure added
    Added client/server/shared/web files to be worked on

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'UiForge - Ked.ss'
+description 'Advanced Blackmarket System for FiveM'
+version '1.0.0'
+github 'https://github.com/Clasher-Ked-ss/UiForge_Blackmarket'
+
+shared_scripts {'shared/*.lua', 'Config.lua'}
+
+client_scripts {'client/*.lua'}
+
+server_scripts {'@oxmysql/lib/MySQL.lua', 'server/*.lua'}
+
+ui_page 'web/index.html'
+
+files {'web/js/*.js', 'web/css/*.css', 'web/index.html', 'web/img/*.png', 'web/img/*.svg', 'web/audio/*.ogg'}
